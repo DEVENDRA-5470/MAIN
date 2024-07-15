7c4f7eddf0c1438dee93b6a6d91868369ddfd592
unknown <ysdevn@gmail.com>
2024-07-15 09:26:16 +0530

    July-15

--- C++ BY-DEV/3_constructor.cpp
+++ C++ BY-DEV/3_constructor.cpp
@@ -1,8 +1,8 @@
-A constructor in C++ is a special member function of a class that initializes objects of the class. It has the same name as the class and does not have a return type. Constructors are called automatically when an object of the class is created.
+// A constructor in C++ is a special member function of a class that initializes objects of the class. It has the same name as the class and does not have a return type. Constructors are called automatically when an object of the class is created.
 
-Types of Constructors
-Default Constructor: A constructor that takes no arguments.
+// Types of Constructors
+// Default Constructor: A constructor that takes no arguments.
 
-Parameterized Constructor: A constructor that takes arguments to initialize the object with specific values.
+// Parameterized Constructor: A constructor that takes arguments to initialize the object with specific values.
 
-Copy Constructor: A constructor that initializes an object using another object of the same class.
+// Copy Constructor: A constructor that initializes an object using another object of the same class.
