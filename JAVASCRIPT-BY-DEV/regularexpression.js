9bb1cd9d9d472da5e68edca76b3f29765fe7c23c
unknown <ysdevn@gmail.com>
2024-07-17 13:13:17 +0530

    July-17

---
+++ JAVASCRIPT-BY-DEV/regularexpression.js
@@ -0,0 +1,99 @@
+// --------------------------------Regular Expression
+// Regular Expression is a sequence of characters that defines a search pattern. It can be used for string matching, searching, and manipulation.
+// Regular expressions can be complex, but they are incredibly useful for tasks like validation, searching, and replacing text in strings. They are widely used in programming, text editors, and many other applications.
+
+// Syntax
+//     /pattern/modifier(s);
+
+// Example:
+// let pattern = /DreAmerInfotech/i; 
+
+// DreAmerInfotech : The pattern to search
+//  /DreAmerInfotech/ : A regular expression
+// i : The modifier to make the search case-insensitive.
+
+
+//------------------------ Modifiers:
+// g : Global search. Find all matches instead of stopping after the first match.
+// i : Case-insensitive search.
+// m : Multiline search. Treat string as multiple lines, common for finding matches across multiple lines
+// u : Unicode. Treat the string as a sequence of Unicode code points.
+// x : Ignore whitespace. Ignores whitespace characters in the regular expression pattern.
+
+
+
+//-------------------------Brackets
+// Brackets are used to find a range of characters:
+// [abc] : Find any of the characters a, b, or c.
+// [a-z] : Find any lowercase letter from a to z.
+// [A-Z] : Find any uppercase letter from A to Z.
+// [0-9] : Find any digit from 0 to 9.
+// (x|y)	Find any of the alternatives specified
+// [a-zA-Z0-9] : Find any letter or digit from a to z,
+//               A to Z, or 0 to 9.
+// [^abc] : Find any character except a, b, or c.
+// [^a-z] : Find any character except lowercase letters from a to z.
+// [^A-Z] : Find any character except uppercase letters from A to Z.
+// [^0-9] : Find any character except digits from 0 to 9.
+
+// [^a-zA-Z0-9] : Find any character except letters or digits from a
+//               to z, A to Z, or 0 to 9.
+// [^a-zA-Z0-9_] : Find any character except letters, digits, or
+//                 underscores from a to z, A to Z, or 0 to 9.
+
+
+// -------------------------Metacharacters
+// Metacharacters are special characters that have a specific meaning in regular expressions. They are used
+// to match patterns in strings. Some common metacharacters are:
+
+//  . : Matches any single character except line terminators.
+//  \ : Escapes special characters and metacharacters.
+//  ^ : Matches the beginning of a string.
+//  $ : Matches the end of a string.
+//  * : Matches zero or more occurrences of the preceding character or group.
+//  + : Matches one or more occurrences of the preceding character or group.
+//  ? : Matches zero or one occurrence of the preceding character or group.
+//  {n} : Matches exactly n occurrences of the preceding character or group.
+//  {n,} : Matches n or more occurrences of the preceding character or group.
+//  {n,m} : Matches at least n and at most m occurrences of the preceding character or
+//          group.
+//  | : Matches either the character or group to the left or right of the | symbol.
+//  ( ) : Groups characters or metacharacters together for matching or for use with other
+//        metacharacters.
+//  [ ] : Defines a character class or a range of characters to match.
+//  \d : Matches any digit character.
+//  \D : Matches any non-digit character.
+//  \s : Matches any whitespace character.
+//  \S : Matches any non-whitespace character.
+//  \w : Matches any word character (alphanumeric or underscore).
+//  \W : Matches any non-word character.
+//  \b : Matches the empty string at the beginning or end of a word.
+//  \B : Matches the empty string within a word.
+//  .	Find a single character, except newline or line terminator
+//  \w	Find a word character
+//  \W	Find a non-word character
+//  \d	Find a digit
+//  \D	Find a non-digit character
+//  \s	Find a whitespace character
+//  \S	Find a non-whitespace character
+//  \b	Find a match at the beginning/end of a word, beginning like this: \bHI, end like this: HI\b
+//  \B	Find a match, but not at the beginning/end of a word
+//  \0	Find a NULL character
+//  \n	Find a new line character
+//  \f	Find a form feed character
+//  \r	Find a carriage return character
+//  \t	Find a tab character
+//  \v	Find a vertical tab character
+
+
+// ------------------------------------Quantifiers
+// n+	Matches any string that contains at least one n
+// n*	Matches any string that contains zero or more occurrences of n
+// n?	Matches any string that contains zero or one occurrences of n
+// n{X}	Matches any string that contains a sequence of X n's
+// n{X,Y}	Matches any string that contains a sequence of X to Y n's
+// n{X,}	Matches any string that contains a sequence of at least X n's
+// n$	Matches any string with n at the end of it
+// ^n	Matches any string with n at the beginning of it
+// ?=n	Matches any string that is followed by a specific string n
+// ?!n	Matches any string that is not followed by a specific string n
---
+++ JAVASCRIPT-BY-DEV/testing.js
@@ -0,0 +1,6 @@
+let arr=[2,5,6,8,34,8,23]
+
+function great(n){
+    return n>10
+}
+console.log(arr.filter(great))
